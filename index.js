3f21059db078ebe86d00cdb8e45d3e80a80bb4a5
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-15 18:36:28 -0700

    added delete and edit buttons but neither function as desired

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Requires
+var bodyParser = require("body-parser");
 var express = require("express");
 var ejsLayouts = require("express-ejs-layouts");
+var teamsDB = require("./models/teamService.js"); //./ because we're looking for a file nad not a module
 
 //Global vars
 var app = express();
@@ -8,11 +10,81 @@ var app = express();
 //Set and Use statements
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(express.static("public"));
 
 //Routes
 app.get("/", function(req, res){
-  res.render("home");
+  	res.render("home");
 });
+
+
+app.get("/teams", function(req, res){
+	var allTeams = teamsDB.getAllTeams();
+  	res.render("teams/index", {teams: allTeams});
+});
+
+
+app.get("/teams/new", function(req,res){
+	res.render("teams/new");
+});
+
+
+app.get("/teams/:name", function(req, res){
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/show", {team: team});
+});
+
+
+app.post("/teams", function(req,res){
+	console.log(req.body);
+	teamsDB.addTeam(req.body); 
+	res.redirect("/teams/" + req.body.name);
+});
+
+app.get("/teams/edit/:name", function(req,res){
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/edit", {team: team});
+
+});
+//andres look below
+app.put("/teams/:name", function(req,res){
+	console.log(req.body);
+	teamsDB.editTeam(req.params.name, req.body); 
+	res.send("Puuut");
+});
+
+app.delete("/teams/:name", function(req,res){
+	teamsDB.deleteTeam(req.params.name); 
+	res.send("Deleted");
+});
+
 
 //Listen
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
-  console.log('Hello from teams-ajax.js!');
-  // TO DO: Define calls to PUT and DELETE routes here
-});
+
+  $("#edit-form").submit(function(e){
+  	e.preventDefault();
+
+  	var teamURL = $(this).attr("action");
+  	var teamData = $(this).serialize();
+
+  	console.log("URL is ", teamURL);
+  	console.log("data ", teamData);
+
+  	$.ajax({
+  		method: "PUT", 
+  		url: teamURL, 
+  		data: teamData
+  	}).done(function(data){
+  		// console.log("success!", data);
+  		window.location = "/teams";
+  	}).fail(function(err){
+  		console.log("Error", err);
+  	}); //end of AJAX
+
+  }); //end of edit form sumit
+  $("#delete-btn").click(function(e){
+  	e.preventDefault();
+  	var teamUrl = $(this).attr("href"); 
+
+  	console.log("stuff is working, URL is ", teamUrl);
+
+  	$.ajax({
+  		method: "DELETE",
+  		url: teamUrl
+  	}).done(function(data){
+  		console.log("success", data);
+  	}).fail(function(err){
+  		console.log("err", err);
+  	});
+  });
+
+}); // end of document ready
